41728a5403f69adaceae93a0b30985ffd254995e
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2022-06-10 23:38:51 +0000

    Added All Components First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let button = document.querySelector(".dice_container");
+button.addEventListener("click", (e) => {
+
+    let endpoint = "https://api.adviceslip.com/advice";
+
+    fetch(endpoint, {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.slip.advice);
+            document.querySelector("#id").innerHTML = `advice ${data.slip.id}`
+            document.querySelector("#advice").innerHTML = data.slip.advice
+        })
+        .catch(err => console.log(err))
+});
